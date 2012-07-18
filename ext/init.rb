0345a9e3d44975e3c424791b856939358b0bcc61
fogus <mefogus@gmail.com>
2012-07-18 14:01:37 -0400

    Output to public

--- ext/init.rb
+++ ext/init.rb
@@ -14,3 +14,5 @@ config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.woff'
 config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.otf'
 config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.ttf'
 config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.svg'
+config['output'] = ["Webgen::Output::FileSystem", "public"]
+
